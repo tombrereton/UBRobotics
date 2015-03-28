84a2ab060bbdc515a892b816354de5c656cc64a0
Yuya Takeuchi <yxt385@bham.ac.uk>
2015-03-28 14:10:59 +0000

    Removed CAD

--- ARDUINO/Prototype code/colour/colour.ino
+++ ARDUINO/Prototype code/colour/colour.ino
@@ -1,5 +1,5 @@
 // Define colour sensor LED pins
-int ledArray[] = {2,3,4};
+int ledArray[] = {2,3,4}; // Digital Pin #
 
 // boolean to know if the balance has been set
 boolean balanceSet = false;
@@ -17,6 +17,9 @@ float blackArray[] = {0,0,0};
 
 //place holder for average
 int avgRead;
+
+//Read Rate
+int speed = 25; //Delay between flashes
 
 void setup(){
  
@@ -51,11 +54,11 @@ void setBalance(){
   //go through each light, get a reading, set the base reading for each colour red, green, and blue to the white array
   for(int i = 0;i<=2;i++){
      digitalWrite(ledArray[i],HIGH);
-     delay(100);
+     delay(speed);
      getReading(5);          //number is the number of scans to take for average, this whole function is redundant, one reading works just as well.
      whiteArray[i] = avgRead;
      digitalWrite(ledArray[i],LOW);
-     delay(100);
+     delay(speed);
   }
   //done scanning white, now it will pulse blue to tell you that it is time for the black (or grey) sample.
    //set black balance
@@ -63,12 +66,12 @@ void setBalance(){
   //go ahead and scan, sets the colour values for red, green, and blue when exposed to black
   for(int i = 0;i<=2;i++){
      digitalWrite(ledArray[i],HIGH);
-     delay(100);
+     delay(speed);
      getReading(5);
      blackArray[i] = avgRead;
      //blackArray[i] = analogRead(2);
      digitalWrite(ledArray[i],LOW);
-     delay(100);
+     delay(speed);
   }
    //set boolean value so we know that balance is set
   balanceSet = true;
@@ -79,13 +82,13 @@ void setBalance(){
 void checkColour(){
     for(int i = 0;i<=2;i++){
      digitalWrite(ledArray[i],HIGH);  //turn or the LED, red, green or blue depending which iteration
-     delay(100);                      //delay to allow CdS to stabalize, they are slow
+     delay(speed);                      //delay to allow CdS to stabalize, they are slow
      getReading(5);                  //take a reading however many times
      colourArray[i] = avgRead;        //set the current colour in the array to the average reading
      float greyDiff = whiteArray[i] - blackArray[i];                    //the highest possible return minus the lowest returns the area for values in between
      colourArray[i] = (colourArray[i] - blackArray[i])/(greyDiff)*255; //the reading returned minus the lowest value divided by the possible range multiplied by 255 will give us a value roughly between 0-255 representing the value for the current reflectivity(for the colour it is exposed to) of what is being scanned
      digitalWrite(ledArray[i],LOW);   //turn off the current LED
-     delay(100);
+     delay(speed);
   }
 }
 void getReading(int times){
