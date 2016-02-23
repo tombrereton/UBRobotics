691437b1e8235389b37dbabed564c6e3b7024bc0
Yuya Takeuchi <comokanu@gmail.com>
2016-02-23 02:00:12 +0000

    Added tutorial notes

--- ARDUINO/libraries/Car/Car.cpp
+++ ARDUINO/libraries/Car/Car.cpp
@@ -1,10 +1,44 @@
 #include "Car.h"
 #include "Arduino.h"
 
+/*
+WIRING THE ROVER 5
 
+1. Make sure the Motor Controller has power (VS and GND connected from battery)
+2. Connect Left track to Channel 1, Right to Channel 2
+3. Take any TWO of the Digital Pins from the Arduino and connect to the controller's CH1 Direction and CH2 Direction headers. Make note of the pin numbers.
+4. Refer to Rover 5 manual online for schematic of its rotary encoders
+5. Connect the Left and Right interrupt jumpers from the Rover 5 to CH1 and CH2 INT pins respectively. Also connect the respective ground pins.
+6. Take any TWO analog out (PWM) pin from the Arduino and connect to the controller's CH1 PWM and CH2 PWM headers. Make note of the pin numbers
+OPTIONAL: Connect a buzzer across any digital pin and GND
+
+The pin numbers you noted will be declared when you make an instance of the Car class. For example:
+
+	Car car(22, 24, 2, 3, 4, 5, 6);
+
+Where the numbers correspond to the pins noted. The meaning of the pins are explained below, in that order
+*/
 
 Car::Car(int direction0, int direction1, int interrupt0, int interrupt1, int pwm0, int pwm1, int buzzer) {
-  
+
+/*
+Variable Declarations
+
+direction0 = Left Track Direction Pin DIGITAL
+direction1 = Right Track Direction Pin DIGITAL
+
+interrupt0 = Interrupt Pin for Left Track DIGITAL
+interrupt1 = Interrupt Pin for Right Track DIGITAL
+
+pwm0 = Left Track PWM Pin ANALOG OUT
+pwm1 = Right Track PWM Pin ANALOG OUT
+
+buzzer = Pin for diagnostic buzzer DIGITAL (If no buzzer just set to unused digital pin)
+*/
+
+
+//Now initialise all PIN NUMBERS to a variable
+
 directionPin[0] = direction0;
 directionPin[1] = direction1;
 
@@ -31,61 +65,68 @@ buzzerPin = buzzer;
 
 
 void Car::displayCounters() {
+//Display encoder count to serial monitor
 Serial.print("Left: ");
 Serial.print(counter[1]);
 Serial.print(", Right: ");
 Serial.println(counter[0]);
 }
 
-void Car::turn(int ticks) {
-isRunning = true;
+void Car::turn(int ticks) { //Method for turning car. 'ticks' defines how many steps it will turn for (calibration required to convert to angle)
+isRunning = true; //Car is running
 
-counter[0] = 0;
+counter[0] = 0; //Reset encoder counter
 
-	if (ticks < 0){
+	if (ticks < 0){ //If number of steps is NEGATIVE, turn CLOCKWISE (set direction pins as required)
 		digitalWrite(directionPin[0], HIGH);
 		digitalWrite(directionPin[1], LOW);
 	}
-	else if (ticks > 0){
+	else if (ticks > 0){ //If number of steps is POSITIVE, turn ANTICLOCKWISE (set direction pins as required)
 		digitalWrite(directionPin[0], LOW);
 		digitalWrite(directionPin[1], HIGH);
 	}
+
+	/*
+	The following code turns the wheels at maximum speed. Experiment with analogWrite() at different speeds of each track to get the smoothest turning
+	*/
 
 	digitalWrite(pwmPin[0], HIGH);
 	digitalWrite(pwmPin[1], HIGH);
 
 	
-
-	while (counter[0] < abs(ticks)){
+	while (counter[0] < abs(ticks)){ //While the magnitude of ticks is below the required number of steps, stay in loop
 		delayMicroseconds(1);
 	}
+
+	//Stop tracks, they have moved sufficient number of steps
 
 	digitalWrite(pwmPin[0], LOW);
 	digitalWrite(pwmPin[1], LOW);
 	Serial.print("End of Cycle.");
-	isRunning = false;
+	isRunning = false; //Important
 }
 
-void Car::move(int speed){
-	if (speed <= 0){ //Reverse
+void Car::move(int speed){ //Move forward at given speed between -255 and 255
+	if (speed <= 0){ //If speed is negative, then REVERSE the direction
 		digitalWrite(directionPin[0], HIGH);	
 		digitalWrite(directionPin[1], HIGH);	
 	}
 
-	else {
+	else { //If positive, make sure direction reverse pins are LOW on both tracks
 		digitalWrite(directionPin[0], LOW);	
 		digitalWrite(directionPin[1], LOW);	
 	}
 
-	if (abs(speed) > 255){
+	if (abs(speed) > 255){ //If magnitude of speed is greater than allowed, cast it back to maximum speed
 		speed = 255;
 	}
 
+	//Set PWM pins to given speed
 	analogWrite(pwmPin[0], abs(speed));
 	analogWrite(pwmPin[1], abs(speed));
 }
 
-void Car::stop(){
+void Car::stop(){ //Stop the car by bringing both PWM pins to LOW
 	digitalWrite(pwmPin[0], LOW);
 	digitalWrite(pwmPin[1], LOW);
 }
--- ARDUINO/libraries/Car/TEST/TEST.ino
+++ ARDUINO/libraries/Car/TEST/TEST.ino
@@ -1,16 +1,29 @@
-#include <Car.h>
-Car car(22, 24, 2, 3, 4, 5, 6);
+#include <Car.h> //Include the library
+
+Car car(22, 24, 2, 3, 4, 5, 6); //Make a new instance of class 'car' called 'Car' with the following pins
+
+/*
+Refer to Car.cpp in the library for the meaning of the numbers
+
+This code is the bare minimum required to get the chassis moving, and a bit more. Use the code as reference to make your own commands.
+*/
 
 void setup() {
-  // put your setup code here, to run once:
-attachInterrupt(0, onPulse, CHANGE);
-car.turn(500);
-  delay (1000);
-car.turn(-200);
+attachInterrupt(0, onPulse, CHANGE); //Attach interrupt pins. Even though we have set up interrupts for both tracks, simultaneously running two interrupts causes problems (workaround needed)
+car.turn(500); //Turn the car anticlockwise by 500 ticks
+  delay (1000); //Pause for 1000 ms (It's good practice to put in breaks between commands)
+car.turn(-200); //Turn the car clockwise by 200 ticks
 }
 
 void loop() {
 }
+
+/*
+
+Following snippet of code is neccesary for the interrupt to work properly. Always include in your sketches.
+
+*/
+
 
 void onPulse() {
   
