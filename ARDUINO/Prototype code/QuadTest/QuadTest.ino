0d48f2e5f0350bf5dbc8c4bc12ed00f08a1d416c
Yuya <comokanu@gmail.com>
2016-06-06 19:27:30 +0100

    Updated Eurobot 2016 Paris

--- ARDUINO/Prototype code/QuadTest/QuadTest.ino
+++ ARDUINO/Prototype code/QuadTest/QuadTest.ino
@@ -1,15 +1,27 @@
+#include <Servo.h>
 #include <MecanumRover.h>
 #include <SPI.h>
 #include <HCMAX7219.h>
 #include <NewPing.h>
 
+int leftArm = 40; //Pin numbers for seashell arms
+int rightArm = 41;
+Servo arm;
+int bias = 30; //Arm will lower less with more bias
+
+int rightAngle = 83;
 int greenButton = 14; //Pin of green button for mode selection
-MecanumRover rover(3,5,7,9,2,4,6,8,18,19,20,21,0,1,2,3,50,0.5); //1-4 Direction Pins, 5-8 PWM Pins, 9-12 Interrupt Pins, 13-16 Current Read Pins, 15 Base Speed, 16 Correction Factor 
+int buzzerPin = 49; //Pin of buzzer
+MecanumRover rover(3,5,7,9,
+                   2,4,6,8,
+                   18,19,20,21,
+                   0,1,2,3,
+                   50,4,49); //1-4 Direction Pins, 5-8 PWM Pins, 9-12 Interrupt Pins, 13-16 Current Read Pins, 15 Base Speed, 16 Correction Factor, 17 Buzzer Pin 
 
 NewPing frontSensor(13, 12, 100);
 NewPing rearSensor(31, 32, 100);
 
-void setup() {
+void setup(){
   // put your setup code here, to run once:
 Serial.begin(9600);
 //pinMode(greenButton, INPUT);
@@ -21,24 +33,27 @@ attachInterrupt(digitalPinToInterrupt(rover.interruptPin[3]), count3, CHANGE);
 
 HCMAX7219 HCMAX7219(10);
 Serial.println(digitalRead(greenButton));
-if (digitalRead(greenButton) == HIGH){
-  HCMAX7219.Clear();
-  HCMAX7219.print7Seg("GREEN", 8); //Green mode
-  HCMAX7219.Refresh();
-  delay(1000);
-}
-else {
-  HCMAX7219.Clear();
-  HCMAX7219.print7Seg("YELLOW", 8); //Yellow mode
-  HCMAX7219.Refresh(); 
-  delay(1000);
-}
 
+leftClose();
+rightClose();
+
+  if (digitalRead(greenButton) == HIGH){ //Check if GREEN BUTTON was pressed
+    HCMAX7219.Clear();
+    HCMAX7219.print7Seg("GREEN", 8); //Green mode
+    HCMAX7219.Refresh();
+    delay(1000);
+  }
+  else {
+    HCMAX7219.Clear();
+    HCMAX7219.print7Seg("PURPLE", 8); //Purple mode
+    HCMAX7219.Refresh(); 
+    delay(1000);
+    eurobotPurple();
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-rover.move(500, frontSensor, rearSensor);
 }
 
 //Keep the following interrupt code simple and non-intensive for accurate operation
@@ -59,3 +74,56 @@ void count3(){
   rover.counter[3] ++;
   rover.encoderChangeTime[3] = millis();
 }
+
+void leftClose(){
+  arm.attach(leftArm);
+    for (int pos = 180; pos >= 0; pos -= 1) { // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+      arm.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(15);                       // waits 15ms for the servo to reach the position
+    }
+  arm.detach();
+}
+
+void leftOpen(){
+  arm.attach(leftArm);
+    for (int pos = 0; pos <= 180 - bias; pos += 1) { // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+      arm.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(15);                       // waits 15ms for the servo to reach the position
+    }
+  arm.detach();
+}
+
+void rightClose(){
+  arm.attach(rightArm);
+    for (int pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+      arm.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(15);                       // waits 15ms for the servo to reach the position
+    }
+  arm.detach();
+}
+
+void rightOpen(){
+  arm.attach(rightArm);
+    for (int pos = 180; pos >= 0 + bias; pos -= 1) { // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+      arm.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(15);                       // waits 15ms for the servo to reach the position
+    }
+  arm.detach();
+}
+
+void eurobotPurple(){
+  rover.strafe(-100);
+  rover.turn(rightAngle);
+  rover.move(400,frontSensor,rearSensor);  
+  rover.turn(-rightAngle);
+  rover.move(500,frontSensor,rearSensor);
+  rover.turn(-rightAngle);
+  rover.move(500,frontSensor,rearSensor);
+  leftOpen();
+  rightOpen();
+}
+
--- ARDUINO/libraries/MecanumRover/MecanumRover.cpp
+++ ARDUINO/libraries/MecanumRover/MecanumRover.cpp
@@ -3,11 +3,15 @@
 #include <SPI.h>
 #include <HCMAX7219.h>
 
+int brakeTime = 20;
+long startTime;
+bool strafing = false; //Are we strafing diagonally EXPERIMENTAL
 int distanceLimit = 40;
 int lastEncoder[4]; //Previous encoder position 
 HCMAX7219 HCMAX7219(10); // SETUP THE LED DISPLAY (Pin 10 = CS 51 = DIN 52 = CLK)
 
-MecanumRover::MecanumRover(int dir0, int dir1, int dir2, int dir3, int pwm0, int pwm1, int pwm2, int pwm3, int int0, int int1, int int2, int int3, int cur0, int cur1, int cur2, int cur3, int base, float factor){
+MecanumRover::MecanumRover(int dir0, int dir1, int dir2, int dir3, int pwm0, int pwm1, int pwm2, int pwm3, int int0, int int1, int int2, int int3, int cur0, int cur1, int cur2, int cur3, int base, float factor, int buzzerPin){
+  buzzer = buzzerPin;
 
   directionPin[0] = dir0; //Initialise direction pins
   directionPin[1] = dir1;
@@ -40,7 +44,7 @@ MecanumRover::MecanumRover(int dir0, int dir1, int dir2, int dir3, int pwm0, int
   }
 }
 
-void MecanumRover::adjustSpeed(){ //Change the speed of the 4 wheels according to the average rotational displacement of all 4 wheels
+void MecanumRover::adjustSpeed(int ticks){ //Change the speed of the 4 wheels according to the average rotational displacement of all 4 wheels
   int averageCount = (counter[0] + counter[1] + counter [2] + counter[3]) / 4;  //Average rotational displacement of all 4 wheels
   
   /*
@@ -56,12 +60,26 @@ void MecanumRover::adjustSpeed(){ //Change the speed of the 4 wheels according t
   */
 
   for (int i = 0; i < 4; i ++){
-    if ((counter[i] > 200) &&(sensorStop == false)){ //To make sure all wheels start up at the same speed avoid correction until a certain displacement, and that the sensor is not detecting anything
+    if ((counter[i] > 0) &&(sensorStop == false)){ //To make sure all wheels start up at the same speed avoid correction until a certain displacement, and that the sensor is not detecting anything
       int correction = averageCount - counter[i]; //Difference of current wheel from average
+      
+        if (correction > speedDeltaCap){
+          correction = speedDeltaCap; //To avoid wheel speeds to increase too much on correction there is a cap
+        }
+        else if (correction < -speedDeltaCap){
+          correction = -speedDeltaCap;
+        }
+
       int adjusted = baseSpeed + correction * correctionFactor; //Change PWM level according to difference
       //Serial.print(adjusted);
       //Serial.print(" ");
-      analogWrite(pwmPin[i], adjusted); //Write to PWM pin
+      //int travelRatio = (abs(ticks) - (averageCount+1)) / abs(ticks); //Percentage difference in travelled distance vs intended
+      if (strafing == true){
+        analogWrite(pwmPin[i], adjusted * 2); //Write to PWM pin
+      }
+      else {
+        analogWrite(pwmPin[i], adjusted); //Write to PWM pin 
+      }
     }
     currentCurrent[i] = analogRead(currentPin[i]);
   }
@@ -84,7 +102,7 @@ void MecanumRover::move(int ticks, NewPing frontSensor, NewPing rearSensor){ //M
     digitalWrite(directionPin[3], LOW);
   }
 
-  int startTime = millis();
+  startTime = millis();
 
   for (int i = 0; i < 4; i ++){
     counter[i] = 0; //Initialise encoder count
@@ -99,52 +117,49 @@ void MecanumRover::move(int ticks, NewPing frontSensor, NewPing rearSensor){ //M
       if ((distance <= distanceLimit) && (distance != 0)){ //When the sensor has nothing in front of it, it gives zero. There is an obstruction when distance < limit and not 0
         for (int i = 0; i < 4; i ++){ //For all 4 wheels
            analogWrite(pwmPin[i], 0); //Off
-           sensorStop = true;
         }
+        sensorStop = true;
+        Serial.print("Stopped moving forwards at: ");
+        Serial.println(millis());
+        //tone(buzzer, 220, 1000);
       }
       else { //Sensor is not obstructing
+        for (int i = 0; i < 4; i ++){ //For all 4 wheels
+           analogWrite(pwmPin[i], baseSpeed); //On
+        }
+        sensorStop = false;
+      }
+    }
+    else if (ticks < 0){
+      int distance = rearSensor.ping_cm();
+
+      if ((distance <= distanceLimit) && (distance != 0)){ //When the sensor has nothing in front of it, it gives zero. There is an obstruction when distance < limit and not 0
+        for (int i = 0; i < 4; i ++){ //For all 4 wheels
+           analogWrite(pwmPin[i], 0); //Off
+        }
+        sensorStop = true;
+        Serial.print("Stopped moving backwards at: ");
+        Serial.println(millis());
+        //tone(buzzer, 220, 1000);
+      }
+      else { //Sensor is not obstructing
+        for (int i = 0; i < 4; i ++){ //For all 4 wheels
+           analogWrite(pwmPin[i], baseSpeed); //On
+        }
         sensorStop = false;
       }
     }
     
 
-    adjustSpeed();
+    adjustSpeed(ticks);
    // checkIfStuck(abs(ticks)); //Check for stuck, if stuck then break out
   }  
-    
+  brake(); 
   for (int i = 0; i < 4; i ++){ //For all 4 wheels
     analogWrite(pwmPin[i], 0); //Off
   }
 
-  long endTime = millis() - startTime;
-  //Print results
-  Serial.println();
-  Serial.print("Movement Length: ");
-  Serial.print(ticks);
-  Serial.println(" ticks");
-
-  Serial.print("Encoder readings: ");
-  for (int i = 0; i < 4; i ++){
-    Serial.print(counter[i]);
-    Serial.print(" ");
-  }
-  Serial.println();
-
-  Serial.print("Motor Voltage Readings: ");
-  for (int i = 0; i < 4; i ++){
-    Serial.print(currentCurrent[i]);
-    Serial.print(" ");
-  }
-
-  Serial.println();
-  Serial.print("Time of movement sequence: ");
-  Serial.print(endTime);
-  Serial.println(" milliseconds");
-  Serial.println();
-
-  HCMAX7219.Clear();
-  HCMAX7219.print7Seg(endTime, 8); //Print Time taken
-  HCMAX7219.Refresh();
+  printResults(ticks);
 }
 
 void MecanumRover::testHardware(){
@@ -224,6 +239,7 @@ void MecanumRover::checkIfStuck(int ticks){
 }
 
 void MecanumRover::strafe(int ticks){ //Move forward/backward a number of ticks
+  strafing = true;
   if (ticks > 0){ //Right strafe
     digitalWrite(directionPin[0], LOW);
     digitalWrite(directionPin[1], HIGH);
@@ -238,55 +254,32 @@ void MecanumRover::strafe(int ticks){ //Move forward/backward a number of ticks
     digitalWrite(directionPin[3], LOW);
   }
 
-  int startTime = millis();
+  startTime = millis();
+
+  /*
+  Strafing only works with all 4 wheels
+  */
 
   for (int i = 0; i < 4; i ++){
     counter[i] = 0; //Initialise encoder count
     analogWrite(pwmPin[i], baseSpeed);
   }
 
-  while(counter[0] + counter[1] + counter[2] + counter[3] < abs(ticks) * 4){ //While total sum of encoders for all wheels is less than the desired distance x4
-    adjustSpeed();
-    checkIfStuck(abs(ticks)); //Check for stuck, if stuck then break out
+  while(counter[0] + counter[1] + counter[2] + counter[3] < abs(ticks) * 4){ //While total sum of encoders for 2 wheels is less than the desired distance x4
+    adjustSpeed(ticks);
+    //checkIfStuck(abs(ticks)); //Check for stuck, if stuck then break out
   }  
     
   for (int i = 0; i < 4; i ++){ //For all 4 wheels
     analogWrite(pwmPin[i], 0); //Off
   }
-
-  long endTime = millis() - startTime;
-  //Print results
-  Serial.println();
-  Serial.print("Strafe Length: ");
-  Serial.print(ticks);
-  Serial.println(" ticks");
-
-  Serial.print("Encoder readings: ");
-  for (int i = 0; i < 4; i ++){
-    Serial.print(counter[i]);
-    Serial.print(" ");
-  }
-  Serial.println();
-
-  Serial.print("Motor Voltage Readings: ");
-  for (int i = 0; i < 4; i ++){
-    Serial.print(currentCurrent[i]);
-    Serial.print(" ");
-  }
-
-  Serial.println();
-  Serial.print("Time of movement sequence: ");
-  Serial.print(endTime);
-  Serial.println(" milliseconds");
-  Serial.println();
-
-  HCMAX7219.Clear();
-  HCMAX7219.print7Seg(endTime, 8); //Print Time taken
-  HCMAX7219.Refresh();
+  brake();
+  strafing = false;
+  printResults(ticks);
 }
 
 void MecanumRover::turn(int ticks){ //Move forward/backward a number of ticks
-  if (ticks > 0){ //CCW
+  if (-ticks > 0){ //CW
     digitalWrite(directionPin[0], HIGH);
     digitalWrite(directionPin[1], LOW);
     digitalWrite(directionPin[2], LOW);
@@ -299,7 +292,7 @@ void MecanumRover::turn(int ticks){ //Move forward/backward a number of ticks
     digitalWrite(directionPin[3], LOW);
   }
 
-  int startTime = millis();
+  startTime = millis();
 
   for (int i = 0; i < 4; i ++){
     counter[i] = 0; //Initialise encoder count
@@ -307,18 +300,23 @@ void MecanumRover::turn(int ticks){ //Move forward/backward a number of ticks
   }
 
   while(counter[0] + counter[1] + counter[2] + counter[3] < abs(ticks) * 4){ //While total sum of encoders for all wheels is less than the desired distance x4
-    adjustSpeed();
-    checkIfStuck(abs(ticks)); //Check for stuck, if stuck then break out
+    adjustSpeed(ticks);
+    //checkIfStuck(abs(ticks)); //Check for stuck, if stuck then break out
   }  
     
   for (int i = 0; i < 4; i ++){ //For all 4 wheels
     analogWrite(pwmPin[i], 0); //Off
   }
+  brake();
+  printResults(ticks);
+}
 
+void MecanumRover::printResults(int ticks){
   long endTime = millis() - startTime;
+  delay(500);
   //Print results
   Serial.println();
-  Serial.print("Strafe Length: ");
+  Serial.print("Length: ");
   Serial.print(ticks);
   Serial.println(" ticks");
 
@@ -346,3 +344,59 @@ void MecanumRover::turn(int ticks){ //Move forward/backward a number of ticks
   HCMAX7219.Refresh();
 }
 
+void MecanumRover::rightDiagonal(int ticks){ //Move forward/backward a number of ticks
+  strafing = true;
+
+  if (ticks > 0){ //Right strafe
+    digitalWrite(directionPin[0], LOW);
+    digitalWrite(directionPin[1], HIGH);
+    digitalWrite(directionPin[2], LOW);
+    digitalWrite(directionPin[3], HIGH);
+  }
+  else { //Left strafe
+
+    digitalWrite(directionPin[0], HIGH);
+    digitalWrite(directionPin[1], LOW);
+    digitalWrite(directionPin[2], HIGH);
+    digitalWrite(directionPin[3], LOW);
+  }
+
+  startTime = millis();
+
+  for (int i = 0; i < 4; i ++){
+    counter[i] = 0; //Initialise encoder count
+  }
+  analogWrite(pwmPin[0], baseSpeed);
+  analogWrite(pwmPin[3], baseSpeed);
+
+  while(counter[0] + counter[1] + counter[2] + counter[3] < abs(ticks) * 2){ //While total sum of encoders for 2 wheels is less than the desired distance x4
+    adjustSpeed(ticks/2); //Half as we using half the wheels
+    //checkIfStuck(abs(ticks)); //Check for stuck, if stuck then break out
+  }  
+    
+  for (int i = 0; i < 4; i ++){ //For all 4 wheels
+    analogWrite(pwmPin[i], 0); //Off
+  }
+  strafing = false;
+  printResults(ticks);
+}
+
+void MecanumRover::brake(){
+  for (int i = 0; i < 4; i ++){ //For all 4 wheels
+    analogWrite(pwmPin[i], 255); //On
+  }
+  for (int i = 0; i < 4; i ++){
+    digitalWrite(directionPin[i], HIGH);
+  }
+  delay(brakeTime);
+  for (int i = 0; i < 4; i ++){
+    digitalWrite(directionPin[i], LOW);
+  } 
+  delay(brakeTime);
+  for (int i = 0; i < 4; i ++){
+    digitalWrite(directionPin[i], HIGH);
+  }  
+  for (int i = 0; i < 4; i ++){ //For all 4 wheels
+    analogWrite(pwmPin[i], 0); //Off
+  }
+}
