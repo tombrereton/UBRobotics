313c3a045acf9e354a211772e1c6056fee160bbf
Dr. Yuya Takeuchi <comokanu@gmail.com>
2016-07-19 02:21:08 +0100

    Making robot sim python

--- RPI/Noob/GUITest.py
+++ RPI/Noob/GUITest.py
@@ -1,4 +1,5 @@
 from Tkinter import *
+from PIL import Image, ImageTk
 import tkFont
 #import RPi.GPIO as GPIO
 
@@ -8,11 +9,21 @@ win.geometry('800x400')
 
 myFont = tkFont.Font(family = 'Helvetica', size = 36, weight = 'bold')
 
+image = Image.open("board.jpg") #import board image
+photo = ImageTk.PhotoImage(image.rotate(90)) #convert for ImageTK format 90deg
+
+board = Label(image=photo)
+board.image = photo
+board.pack()
+board.place(x=0,y=0) #must come after pack()
+
 def exitProgram():
     print("Exit")
     win.quit()
 
 exitButton = Button(win, text = "Exit", font = myFont, command = exitProgram, height = 2, width = 8)
-exitButton.pack(side = BOTTOM)
+#exitButton.pack(side = BOTTOM)
+
+
 
 mainloop()
