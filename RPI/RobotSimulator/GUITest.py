53642a39a9f846224d4c5f6d994a1f29b206f9cf
Dr. Yuya Takeuchi <comokanu@gmail.com>
2016-08-11 19:16:45 +0000

    Added distance and angle measuring tool, rewriting config save script

--- RPI/RobotSimulator/GUITest.py
+++ RPI/RobotSimulator/GUITest.py
@@ -1,29 +1,28 @@
+#Python 2.7 compatible, do not use Python 3. Make sure host computer has tkinter and PIL libraries. On debian-based OSes use 'apt-get' to acquire libraries from the internet
+
 from Tkinter import *
 from PIL import Image, ImageTk
 import tkFont
 import ConfigParser #to save settings
 import os.path #check file exists
 import time
+import numpy
 from eurobot import *
-global primaryRobot
+global primaryRobot, datum, boundsize, firstPoint, measureLine, isRecording, angleLine, secondPoint #make global so all functions can access it
 
-Config = ConfigParser.ConfigParser()
+isRecording = False #Path recording is initially off
+
+Config = ConfigParser.ConfigParser() #init object for storing .ini config files
 filepath = "config.ini"
 #import RPi.GPIO as GPIO
 
-#Init config file
-if not os.path.exists(filepath):
-    print("Config file does not exist, making new one")
-    cfgfile = open(filepath,'w')
-    Config.add_section('Controller')
-else:
-    print("Reading config file")
-    Config.read(filepath)
-
 datum = [0,0] #Zero co-ordinate of the board (bottom right)
 boundsize = [0,0] #Size of boundary
-datumRadius = 10
+datumRadius = 10 #radius of datum marker (circle)
 logoPadding = 20 #horizontal padding around controller logo
+pixeltoCM = [0,0] #Used to calibrate pixels to actual distance in cm
+firstPoint = [0,0] #Co-ordinate of first point of measuring line
+secondPoint = [0,0]
 
 win = Tk() #Create a Tkinter window object for ARENA
 win.title("Robot Simulator")
@@ -31,59 +30,64 @@ win.geometry('800x600')
 
 myFont = tkFont.Font(family = 'Helvetica', size = 12, weight = 'bold')
 w = Canvas(win) #canvas module to hold images, shapes
+
+#hbar=Scrollbar(win,orient=HORIZONTAL)
+#hbar.pack(side=BOTTOM,fill=X)
+#hbar.config(command=w.xview)
+#vbar=Scrollbar(win,orient=VERTICAL)
+#vbar.pack(side=RIGHT,fill=Y)
+#vbar.config(command=w.yview)
+
+#w.config(xscrollcommand=hbar.set,yscrollcommand=vbar.set)
+
 w.pack(expand=YES,fill=BOTH)
+measureLine = w.create_line(0,0,0,0)
+angleLine = w.create_line(0,0,0,0)
 
 image = Image.open("board.jpg") #import board image
 photo = ImageTk.PhotoImage(image.rotate(90)) #convert for ImageTK format 90deg
 height, width = image.size #inverted order of height width as the image is rotated 90deg
-board = w.create_image(width/2,height/2,image = photo);
-print(width)
-print(height)
+board = w.create_image(width/2,height/2,image = photo); #add image object to main canvas 'w'
+win.geometry(str(width)+"x"+str(height))
+#print(width)
+#print(height)
 
-datum = [width,height]
+datum = [width,height] #store datum and arena boundary sizes to variable
 boundsize = [width,height]
 
-def drawcircle(canv,x,y,rad):
-    canv.create_oval(x-rad,y-rad,x+rad,y+rad,width=0,fill='blue')
+def drawcircle(canv,x,y,rad): #use this to draw circles giving the CANVAS, X & Y of the centre and the radius
+    global targetPoint
+    targetPoint = canv.create_oval(x-rad,y-rad,x+rad,y+rad,width=0,fill='blue')
+    
+def drawcirclered(canv,x,y,rad): #use this to draw circles giving the CANVAS, X & Y of the centre and the radius
+    global targetPoint2
+    targetPoint2 = canv.create_oval(x-rad,y-rad,x+rad,y+rad,width=0,fill='red')
+
+def drawcirclegreen(canv,x,y,rad): #use this to draw circles giving the CANVAS, X & Y of the centre and the radius
+    global targetPoint3
+    targetPoint3 = canv.create_oval(x-rad,y-rad,x+rad,y+rad,width=0,fill='green')
+
+
+drawcirclered(w,0,0,0)
+drawcirclegreen(w,0,0,0)
 
 #Mark datum
 datumPoint = drawcircle(w,width,height,datumRadius)
 #mark boundary walls
 boundary = w.create_rectangle(0,0,width,height,outline='red',width=3)
 
-def exitProgram():
-    print("Exit")
-    win.quit()
-
-exitButton = Button(win, text = "Exit", font = myFont, command = exitProgram, height = 2, width = 8)
-#exitButton.pack(side = BOTTOM)
-
 #Controller Window
 win2 = Toplevel() #Create a Tkinter window object for controller TOPLEVEL when you have more than one
 win2.title("Global Settings")
-win2.geometry('300x400')
+win2.geometry('500x400')
 w2 = Canvas(win2,bg="white")
-w2.pack(expand=YES,fill=BOTH)
+w2.pack(expand=YES,fill=BOTH) #let it fill the entire window
 
-#image = Image.open("logo.jpg") #import logo image
-#width,height = image.size
-#aspect = width/height
-#logophoto = ImageTk.PhotoImage(image = image.resize((300-logoPadding*2,(300-logoPadding*2)/aspect))) #convert for ImageTK format, resize proportionally
-#w2.create_image(150,50,image=logophoto)
-
-#load .ini data
-if os.path.exists(filepath):
-    print("Loading")
-    datum = eval(Config.get('Controller','datum'),{},{})
-    arenadimension = eval(Config.get('Controller','boundsize'),{},{}) #Parse to a new array
-    width = arenadimension[0]
-    height = arenadimension[1]
-
-#Datum
+#Datum text label and input fields, load the input fields with .ini data if exists
 t1 = Label(w2,text="Datum X: ").grid(row=0,column=0)
 e1 = Entry(w2) #DatumX
-e1.insert(0,datum[0])
-e1.grid(row=0,column=1)
+e1.insert(0,datum[0]) #set input field
+e1.grid(row=0,column=1) #grid allows for easy positioning of canvas elements
 t2 = Label(w2,text="Datum Y: ").grid(row=1,column=0)
 e2 = Entry(w2) #DatumY
 e2.insert(0,datum[1])
@@ -97,31 +101,46 @@ t4 = Label(w2,text="Arena width: ").grid(row=4,column=0)
 e4 = Entry(w2) #DatumY
 e4.insert(0,height)
 e4.grid(row=4,column=1)
+instructions = Label(w2,bg="white",text="LMB to place position marker, RMB to place distance marker.").grid(row=6,column=0,columnspan=2)
+instructions = Label(w2,bg="white",text="Place a third point with MMB after placing the two markers to measure an angle.").grid(row=7,column=0,columnspan=2)
 
-def saveSettings(thedatum,thebound):
-    cfgfile = open(filepath,'w')
-    Config.set('Controller','datum',thedatum)
-    Config.set('Controller','boundsize',thebound)
-    Config.write(cfgfile)
-    cfgfile.close()
+def saveSettings(thedatum,thebound): #autosave to .ini
+    return
     
 def setArena():
-    datum = [int(e1.get()),int(e2.get())]
-    boundsize = [int(e3.get()),int(e4.get())]
+    global datum, boundsize, pixeltoCM
+    datum = [eval(e1.get()),eval(e2.get())] #get datum and boundary size from text input fields above
+    boundsize = [eval(e3.get()),eval(e4.get())]
+    pixeltoCM[0] = float(e3.get()) / 300 #Horizontal length is 300 cm
+    pixeltoCM[1] = float(e4.get()) / 200 #Vertical length is 200 cm
     print("Settings updated.")
-    print datum
-    print boundsize
+    #print pixeltoCM
+    #print datum
+    #print boundsize
     #now redraw datum and boundary
     w.delete("all") #delete all images from previous setting
     height, width = image.size
-    board = w.create_image(width/2,height/2,image = photo)
-    datumPoint = drawcircle(w,datum[0],datum[1],datumRadius)
-    boundary = w.create_rectangle(datum[0]-boundsize[0],datum[1]-boundsize[1],datum[0],datum[1],outline='red',width=3)
-    saveSettings(datum,boundsize)
+    board = w.create_image(width/2,height/2,image = photo) #draw board picture
+    datumPoint = drawcircle(w,datum[0],datum[1],datumRadius) #draw datum point as circle
+    boundary = w.create_rectangle(datum[0]-boundsize[0],datum[1]-boundsize[1],datum[0],datum[1],outline='red',width=1) #draw arena boundaries
+    saveSettings(datum,boundsize) #function above
     
-setter = Button(w2, text="Set new datum and arena size", command=setArena)
+setter = Button(w2, text="Set new datum and arena size", command=setArena) #button to set new arena sizes
 setter.grid(row=5,column=0)
-setArena() #load config
+
+def setRecord(): #Begin/Finish path recording
+    global isRecording
+    
+    if isRecording == False:
+        tkMessageBox.showinfo('Recording begin','Path recording activated, left click on arena to set path co-ordinates')
+    else:
+        isSave = tkMessageBox.askokcancel('Recording stop','Save path?')
+
+    isRecording = not isRecording 
+
+setRecord = Button(w2, text="Toggle Path Recording", command=setRecord) #button to draw robot path by clicking co-ordinates
+setRecord.grid(row=5,column=1)
+setArena() #auto-load config when program boots
 
 #Tests
 #test = Eurobot(60,50,[400,400],-90,w)
@@ -131,7 +150,7 @@ setArena() #load config
 #dada = w.create_image(test.position[0],test.position[1], image = test.photo)
 
 win3 = Toplevel() #Create a Tkinter window object for controller TOPLEVEL when you have more than one
-win3.title("Primary Robot Controller")
+win3.title("Primary Robot Controller") #set all primary robot parameters
 win3.geometry('300x400')
 w3 = Canvas(win3,bg="white")
 w3.pack(expand=YES,fill=BOTH)
@@ -157,25 +176,95 @@ e9 = Entry(w3)
 e9.insert(0,0)
 e9.grid(row=4,column=1)
 
-def setPrimary():
-    global primaryRobot
-    primaryRobot = Eurobot(int(e8.get()),int(e9.get()),[int(e5.get()),int(e6.get())],int(e7.get()),w)
-    #primaryRobot.translate(100)
+def setPrimary(): #function to 'build' primary robot according to text input
+    global primaryRobot #make sure the instance of class is created at the global level for full access
+    primaryRobot = Eurobot(int(e8.get()),int(e9.get()),[int(e5.get()),int(e6.get())],int(e7.get()),w) #set parameters according to input fields
+    #primaryRobot.abstranslate([100,100])
 
-Label(w3,text="", bg="white").grid(row=5,column=0)
-activate1 = Button(w3, text="Build primary robot", command=lambda: setPrimary())
+Label(w3,text="", bg="white").grid(row=5,column=0) #blank space
+activate1 = Button(w3, text="Build primary robot", command=setPrimary) #button to set primary robot parameters
 activate1.grid(row=6,column=0)
 
-def flipPrimary():
+def flipPrimary(): #flip primary robot to other side of arena
     global primaryRobot
+    primaryRobot = Eurobot(primaryRobot.track,primaryRobot.diameter,primaryRobot.position,primaryRobot.angle,w) #must initialise again to retrieve the instance (needs fix)
+    
     try:
         primaryRobot
     except NameError:
         tkMessageBox.showerror('Unable to flip robot!','Robot has not been built!')
     else:
+        primaryRobot.rotate(180)
         print "Flipped primary robot"
+        reflect = datum[0] - boundsize[0]/2 #X co-ordinate of line of reflection
+        primaryRobot.abstranslate([2*(reflect - primaryRobot.position[0]),0])
     
 flip1 = Button(w3, text="Flip sides", command=flipPrimary)
 flip1.grid(row=6,column=1)
+Label(w3,text="", bg="white").grid(row=7,column=0) #blank space
+
+#Some reason unable to animate robot unless it is done in the same function that created it
+moveButton = Button(w3, text="Move to co-ordinate")
+moveButton.grid(row=8,column=0)
+
+def printcoords(event):
+    global targetPoint,pixeltoCM,datum,firstPoint,targetPoint2,measureLine,targetPoint3, reddot
+
+    if isRecording == False:
+        reddot = False
+        firstPoint = [event.x,event.y]
+        w.delete(targetPoint)
+        w.delete(targetPoint2)
+        w.delete(targetPoint3)
+        w.delete(measureLine)
+        w.delete(angleLine)
+        print("Clicked co-ordinate (X,Y):")
+        print(int(-(event.x - datum[0])/pixeltoCM[0]),int(-(event.y - datum[1])/pixeltoCM[1]))
+        print;
+        drawcircle(w,event.x,event.y,5)
+
+def linecoords(event):
+    global firstPoint,pixeltoCM,datum,targetPoint2,measureLine,isRecording,targetPoint3,secondPoint,reddot,linevector
+    
+    if isRecording == False:
+        reddot = True
+        secondPoint = [event.x,event.y]
+        w.delete(targetPoint2)
+        w.delete(targetPoint3)
+        w.delete(measureLine)
+        w.delete(angleLine)
+        measureLine = w.create_line(event.x,event.y,firstPoint[0],firstPoint[1])
+        linevector = [secondPoint[0]-firstPoint[0],secondPoint[1]-firstPoint[1]]
+        print("Distance measurements [dX dY]:")
+        distance = [int(-(firstPoint[0] - event.x)/pixeltoCM[0]),int((firstPoint[1] - event.y)/pixeltoCM[1])]
+        magnitude =  numpy.array([round(-(firstPoint[0] - event.x)/pixeltoCM[0],0),round(-(firstPoint[1] - event.y)/pixeltoCM[1],0)])
+        angle = round(math.degrees(math.atan2(distance[0],(distance[1] + 0.0001))),0)
+        print "[" + str(distance[0]) + " " + str(distance[1]) + "]" + " heading: " + str(-angle) + "deg from N"
+        #UNCOMMENT BELOW FOR DISTANCE MEASUREMENT MAGNITUDE
+        print round(numpy.linalg.norm(magnitude),2)
+        print;
+        drawcirclered(w,event.x,event.y,5)
+
+def anglemeasure(event):
+    global targetPoint3, angleLine, secondPoint, isRecording, reddot, linevector
+
+    if isRecording == False and reddot == True:
+        w.delete(targetPoint3)
+        w.delete(angleLine)
+        drawcirclegreen(w,event.x,event.y,5)
+        angleLine = w.create_line(event.x,event.y,secondPoint[0],secondPoint[1])
+        secondvector = [event.x - secondPoint[0],event.y - secondPoint[1]]
+
+        #Dot product of A&B = |A||B|cos(theta)
+        dot = numpy.vdot(linevector,secondvector)
+        costheta = dot/numpy.linalg.norm(linevector)/numpy.linalg.norm(secondvector)
+        theta = math.acos(costheta)
+        print "Angle BlueRedGreen:"
+        print str(180-int(math.degrees(theta))) + "deg"
+        print
+        
+w.bind("<Button 1>",printcoords)
+w.bind("<Button 3>",linecoords)
+w.bind("<Button 2>",anglemeasure)
 
 mainloop()
--- RPI/RobotSimulator/eurobot.py
+++ RPI/RobotSimulator/eurobot.py
@@ -12,30 +12,38 @@ class Eurobot(object):
             self.track = track #Distance between wheels
             self.diameter = diameter #Diameter of wheels
             self.position = position #init position
-            self.angle = angle
+            self.angle = angle #init heading of robot where 0 is NORTH and is positive counter-clockwise
             self.image = Image.open("robot.jpg") #import image
-            self.image = self.image.resize((track, diameter), Image.ANTIALIAS)
-            photo = ImageTk.PhotoImage(self.image.rotate(self.angle+180)) #convert for ImageTK format        
-            self.robot = self.canvas.create_image(self.position[0],self.position[1],image = photo)
+            self.image = self.image.resize((track, diameter), Image.ANTIALIAS) #resize image to match track and wheel diameter
+            photo = ImageTk.PhotoImage(self.image.rotate(self.angle+180)) #convert for ImageTK format (180 deg such that the feet of the robot picture is the forward direction)
+            self.robot = self.canvas.create_image(self.position[0],self.position[1],image = photo) #display the PhotoImage object
 
             print "Created primary robot - Track: %d, Diameter: %d, Heading: %d Position: [%d, %d]" % (self.track,self.diameter,self.angle,self.position[0],self.position[1])
+            print
         else:
             tkMessageBox.showerror('Robot creation failed!','Invalid parameters detected!')
 
-    def translate(self,distance):
+    def translate(self,distance): #move according to robot heading and distance 
         print "Robot at angle: %f" % self.angle
-        x_vector = -math.sin(math.radians(self.angle))
-        y_vector = math.cos(math.radians(self.angle))
+        x_vector = -math.sin(math.radians(self.angle)) #Work out X and Y displacement vectors according to heading
+        y_vector = -math.cos(math.radians(self.angle))
         print "Vector: [%f, %f]" % (x_vector, y_vector)
 
-        self.position[0] += distance * x_vector
-        self.position[1] -= distance * y_vector
-        print "Moving from: [%d, %d] to [%d, %d]" % (self.position[0] - distance * x_vector,self.position[1] + distance *y_vector,self.position[0],self.position[1])
+        self.position[0] += distance * x_vector #add to current position
+        self.position[1] += distance * y_vector
+        print "Moving from: [%d, %d] to [%d, %d]" % (self.position[0] - distance * x_vector,self.position[1] - distance *y_vector,self.position[0],self.position[1]) #written like this as python doesn't have constants
      
-        self.canvas.move(self.robot, distance * x_vector, -distance * y_vector)
+        self.canvas.move(self.robot, distance * x_vector, distance * y_vector) #move by given vector
+
+    def abstranslate(self,distance): #move regardless of heading
+        self.position[0] += distance[0]
+        self.position[1] += distance[1]
+        self.canvas.move(self.robot, distance[0], distance[1]) #move by given vector
+        print self.position
         
     def rotate(self,degrees):
         self.angle += degrees
         print "Rotated by %d to a heading of %d" % (degrees, self.angle) 
         photo = ImageTk.PhotoImage(self.image.rotate(self.angle))
         self.robot = self.canvas.create_image(self.position[0],self.position[1],image = photo)
+        
